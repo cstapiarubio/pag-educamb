69d1e0d12b9886b61ca2682991333158b6d445b6
cstapiarubio <cstapiarubio@gmail.com>
2017-07-04 20:54:34 -0400

    estructura preliminar de la pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-main.js
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+    $("#myCarousel").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+    $("#myCarousel").carousel("prev");
+});
